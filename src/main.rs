f666c95eeeb3cb4cea8788ca5b3877063742d0e3
Daniel Stiner <daniel.stiner@gmail.com>
2021-01-25 23:41:39 -0800

    Set mouse position

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate conv;
+use conv::*;
+use enigo::{Enigo, MouseControllable};
+
 fn main() {
-    println!("Hello, world!");
+    let (xp, yp) = (0.5, 0.5);
+
+    let (ws, hs) = Enigo::main_display_size();
+    let w = f64::value_from(ws).unwrap();
+    let h = f64::value_from(hs).unwrap();
+
+    let mut enigo = Enigo::new();
+    enigo.mouse_move_to((xp*w).round() as i32, (yp*h).round() as i32);
 }
